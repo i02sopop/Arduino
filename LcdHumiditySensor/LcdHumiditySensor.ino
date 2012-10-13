497d9a815647c21fa3eb6898fda3191be8e85c51
Pablo Alvarez de Sotomayor Posadillo <palvarez@ritho.net>
2012-10-13 18:22:57 +0200

    Added analog temperature sensor

--- LcdHumiditySensor/LcdHumiditySensor.ino
+++ LcdHumiditySensor/LcdHumiditySensor.ino
@@ -10,6 +10,7 @@ const int serialBaudRate = 9600;
 const int sensorReadIntervalMs = 5000;
 int millisSinceLastRead = 0;
 int sensorPin = 2;
+int tempPin = 0;
 
 // Microseconds since last state transition. Used to check timing, which is required
 // to read bits and helpful for error checking in other states.
@@ -31,16 +32,6 @@ char errorMsgBuf[256];
 
 // and a flag to store error state during sensor read/processing
 boolean errorFlag = false;
-
-// Clear all the sensor reader state before we start reading
-void initState() {
-	detachInterrupt(sensorPin - 2);
-	for(int i = 0; i < 86; i++) { timings[i] = 0; }
-	errorFlag = false;
-	lastTransitionMicros = micros();
-	signalLineChanges = 0;
-	// TODO: memory barrier to guarantee visibility of timings array
-}
   
 // Interrupt service routine to capture timing between signal pulses and record
 // them. The pulse sequence will be analysed once it's all been captured so we don't
@@ -61,8 +52,7 @@ void flagError() {
 	errorFlag = true;
 
 	// then write the error msg
-	lcd.setCursor(0, 0);
-	lcd.print(errorMsgBuf);
+	Serial.println(errorMsgBuf);
 }
 
 int shiftNextBit(int oldValue, int pulseMicros, int timingIndex) {
@@ -79,24 +69,24 @@ int shiftNextBit(int oldValue, int pulseMicros, int timingIndex) {
 	}
 }
 
-// Ensure the passed pulse length is within an acceptable range for a pre-bit-transmission
-// low pulse. It should be 45-55 us, but the datasheet is full of lies and we seem to need a tolerance
-// around 35-75us to get it to actually work.
-void checkPreBitLowPulse(int pulseMicros, int timingIndex) {
-	if (pulseMicros <= 35 || pulseMicros >= 75) {
-		sprintf(errorMsgBuf, "Low pulse before bit transmit (%i us) outside 45-70us tolerance at timing idx %i", pulseMicros, timingIndex);
-		flagError();
-	}
-}
-
-int readnbits(int * timingsIdx, int nbits) {
-	const int * t = timings + *timingsIdx;
-	const int * tStop = t + nbits*2;
+int readnbits(int *timingsIdx, int nbits) {
+	const int *t = timings + *timingsIdx;
+	const int *tStop = t + nbits * 2;
 	int result = 0;
+	int pulseMicros = *t;
 
 	while(t != tStop) {
-		checkPreBitLowPulse( *(t++), (*timingsIdx)++ );
-		result = shiftNextBit( result, *(t++), (*timingsIdx)++ );
+		// Ensure the passed pulse length is within an acceptable range for a pre-bit-transmission
+		// low pulse. It should be 45-55 us, but the datasheet is full of lies and we seem to need a tolerance
+		// around 35-75us to get it to actually work.
+		pulseMicros = *(t++);
+		(*timingsIdx)++;
+		if(pulseMicros <= 35 || pulseMicros >= 75) {
+			sprintf(errorMsgBuf, "Low pulse before bit transmit (%i us) outside 45-70us tolerance at timing idx %i", pulseMicros, *timingsIdx);
+			flagError();
+		}
+
+		result = shiftNextBit(result, *(t++), (*timingsIdx)++);
 	}
 
 	return result;
@@ -149,7 +139,7 @@ void analyseTimings(int * temperature_decidegrees_c, int * rel_humidity_deciperc
 	}
 
 	// If bit 16 is set in the temperature, temperature is negative.
-    	if(temp16 & (1<<15))
+	if(temp16 & (1<<15))
         temp16 = -(temp16 & (~(1<<15)));
 
     if(!errorFlag) {
@@ -191,7 +181,7 @@ boolean requestSensorRead() {
     // If it hasn't done so within 200 microseconds assume it didn't see our
     // pulse and enter error state.
     int pulseLength = pulseIn(sensorPin, LOW, 200);
-    if(pulseLength == 0) {
+    if(!pulseLength) {
         sprintf(errorMsgBuf, "Sensor read failed: Sensor never pulled line LOW after initial request");
         flagError();
         return false;
@@ -200,7 +190,7 @@ boolean requestSensorRead() {
     // The sensor has pulled the line low. Wait for it to return to HIGH.
     delayMicroseconds(5);
     pulseLength = pulseIn(sensorPin, HIGH, 200);
-    if(pulseLength == 0) {
+    if(!pulseLength) {
         sprintf(errorMsgBuf, "Sensor read failed: Sensor didn't go back HIGH after LOW response to read request");
         flagError();
         return false;
@@ -225,17 +215,21 @@ boolean requestSensorRead() {
 // Humidity is returned in percentage relative humidity times ten. Divide by ten
 // to get the real relative humidity. Eg a return of 631 means 63.1% humidity.
 //
-boolean readSensor(int * temperature_decidegrees_c, int * rel_humidity_decipercent) {
-    initState();
+boolean readSensor(int *temperature_decidegrees_c, int *rel_humidity_decipercent) {
+	// Clear all the sensor reader state before we start reading
+	detachInterrupt(sensorPin - 2);
+	for(int i = 0; i < 87; i++) { timings[i] = 0; }
+	errorFlag = false;
+	lastTransitionMicros = micros();
+	signalLineChanges = 0;
+	// TODO: memory barrier to guarantee visibility of timings array
 
     // Install the state transition interrupts so we see our own request pulses
     // and all the sensor replies,
     attachInterrupt(sensorPin - 2, sensorLineChange, CHANGE);
 
     // Send a sensor read request pulse (7000ms LOW) and check that the sensor replies with 80ms low
-    if (!requestSensorRead()) {
-        flagError();
-    } else {
+    if(requestSensorRead()) {
         // collect the sensor line pulses then stop capturing interrupts on the line
         // once we either time out or get all the sensor pulses we expect.
         const long startMillis = millis();
@@ -257,33 +251,56 @@ boolean readSensor(int * temperature_decidegrees_c, int * rel_humidity_deciperce
     return !errorFlag;
 }
 
+int readAnalogTemp() {
+	int tempc = 0; // temperature variables
+	int samples[8]; // variables to make a better precision
+	int i;
+
+	for(i = 0; i < 8; i++) {
+		// gets 8 samples of temperature
+		// samples[i] = analogRead(tempPin) * 5.0 * 6.5 / 1024.0;
+		samples[i] = (analogRead(tempPin) * 5.0 * 100 / 1024.0) - 273;
+		tempc = tempc + samples[i];
+		delay(1000);
+	}
+
+	tempc = tempc/8.0; // better precision
+	return tempc;
+}
+
 void setup() {
     // set up the LCD's number of columns and rows: 
     lcd.begin(16, 2);
 
     // INPUT mode is the default, but being explicit never hurts
     pinMode(sensorPin, INPUT);
+
     // Pull the sensor pin high using the Arduino's built-in 20k
     // pull-up resistor. The sensor expects it to be pulled high
     // for the first 2s and kept high as its default state.
     digitalWrite(sensorPin, HIGH);
+
     // The sensor expects no communication for the first 2s, so delay
     // entry to the polling loop to give it tons of time to warm up.
     delay(5000);
+
     // Now ensure the serial port is ready
     Serial.begin(serialBaudRate);
     Serial.println("Sensor initialized, Ready for first reading");
 }
 
 void loop() {
-    int temperature = 0;
-    int humidity = 0;
+	int maxT = -100, minT = 100; // to start max/min temperature
+    int temperature = 0, humidity = 0, tempc = readAnalogTemp();
     boolean success = readSensor(&temperature, &humidity);
+
+	if(tempc > maxT) { maxT = tempc; } // set max temperature
+	if(tempc < minT) { minT = tempc; } // set min temperature
 
     if (success) {
         char line1[16];
         char line2[16];
-        sprintf(line1, "Temp %i.%i", temperature / 10, abs(temperature % 10));
+        sprintf(line1, "Temp %i.%i %i", temperature / 10, abs(temperature % 10), tempc);
         sprintf(line2, "Hum Rel %i.%i", humidity / 10, humidity % 10);
 
 		// set the cursor to column 0, line 1
